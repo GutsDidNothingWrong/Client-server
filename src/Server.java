c9745dd2df5ccda9f9bd966f43b24e6526c5ee01
Ilya Makarov <Madness1337229@gmail.com>
2022-05-28 21:49:02 +0300

    Fixes in Server and Client

--- src/Server.java
+++ src/Server.java
@@ -11,12 +11,16 @@ public class Server {
         int port = 8080;
 
         ServerSocket serverSocket = new ServerSocket(port);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        System.out.println("New connection accepted");
-        final String name = in.readLine();
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("New connection accepted");
+            final String name = in.readLine();
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        }
     }
 }
+
