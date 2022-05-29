03e14dee1b68c92d93c14bfebe09353560cd452e
Ilya Makarov <Madness1337229@gmail.com>
2022-05-29 20:10:13 +0300

    Added try-catch into Server.java

--- src/Server.java
+++ src/Server.java
@@ -13,14 +13,15 @@ public class Server {
         ServerSocket serverSocket = new ServerSocket(port);
 
         while (true) {
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            System.out.println("New connection accepted");
-            final String name = in.readLine();
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            try (Socket clientSocket = serverSocket.accept()) {
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("New connection accepted");
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
         }
     }
 }
-
